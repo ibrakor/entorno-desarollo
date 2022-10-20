ae9b15c77cf03c9b1473cd7d0b69e8eb1f40e6ec
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2022-10-21 00:41:15 +0200

    Introducidos varios metodos en main

--- src/com/ibrakor/PcComponentes/Main.java
+++ src/com/ibrakor/PcComponentes/Main.java
@@ -19,7 +19,7 @@ public class Main {
          */
 
         PlacaBase msi = new PlacaBase();
-        msi.setId(3);
+        msi.setId(2);
         msi.setMarca("MSI Mortar");
         msi.setChipset("AM4");
         msi.setPrecio("150 €");
@@ -31,22 +31,72 @@ public class Main {
          */
 
         TarjetaGrafica nvidia = new TarjetaGrafica();
-        nvidia.setId(4);
+        nvidia.setId(3);
         nvidia.setVram("12 GB");
         nvidia.setNombre("RTX 2060");
         nvidia.setPrecio("400 €");
         System.out.println(nvidia.getVram());
         System.out.println(nvidia.getNombre());
         System.out.println(nvidia.getPrecio());
+        /**
+         * Salida RAM
+         */
+        RAM corsairam = new RAM();
+        corsairam.setId(4);
+        corsairam.setNombre("Corsair Vengeance");
+        corsairam.setPrecio("150 €");
+        corsairam.setReloj("3200 Mhz");
+        System.out.println(corsairam.getNombre());
+        System.out.println(corsairam.getPrecio());
+        System.out.println(corsairam.getReloj());
 
-        RAM corsair = new RAM();
-        corsair.setId(5);
-        corsair.setNombre("Corsair Vengeance");
-        corsair.setPrecio("150 €");
-        corsair.setReloj("3200 Mhz");
+        /**
+         * Salida Torre
+         */
+        Torre aerocool = new Torre();
+        aerocool.setId(5);
+        aerocool.setNombre("Aerocool");
+        aerocool.setPrecio("100 €");
+        aerocool.setModelo("P7 C1");
+        System.out.println(aerocool.getNombre());
+        System.out.println(aerocool.getPrecio());
+        System.out.println(aerocool.getModelo());
+        /**
+         * Disipador
+         */
+        Disipador coolerMaster = new Disipador();
+        coolerMaster.setId(6);
+        coolerMaster.setMarca("Cooler Master Hyper 212");
+        coolerMaster.setChipset("AM4, AM2");
+        coolerMaster.setPrecio("45 €");
+        System.out.println(coolerMaster.getMarca());
+        System.out.println(coolerMaster.getChipset());
+        System.out.println(coolerMaster.getPrecio());
+        /**
+         * Fuente de alimentacion
+         */
+        FuenteAlimentacion corsair = new FuenteAlimentacion();
+        corsair.setId(7);
+        corsair.setNombre("Corsair");
+        corsair.setModelo("RM750 80 Plus Gold");
+        corsair.setPotencia("750W");
+        corsair.setPrecio("120 €");
         System.out.println(corsair.getNombre());
+        System.out.println(corsair.getModelo());
+        System.out.println(corsair.getPotencia());
         System.out.println(corsair.getPrecio());
-        System.out.println(corsair.getReloj());
-
+        /**
+         * Disco duro
+         */
+        DiscoDuro barracuda = new DiscoDuro();
+        barracuda.setId(8);
+        barracuda.setNombre("Seagate");
+        barracuda.setModelo("Barracuda 3.5´ SATA 3");
+        barracuda.setPrecio("50 €");
+        barracuda.setCapacidad("2 TB");
+        System.out.println(barracuda.getNombre());
+        System.out.println(barracuda.getModelo());
+        System.out.println(barracuda.getPrecio());
+        System.out.println(barracuda.getCapacidad());
     }
 }
